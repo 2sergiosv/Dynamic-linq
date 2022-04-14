c7123c218fb5f04cf43a14163f4a2556079d67f5
2sergiosv <42967347+2sergiosv@users.noreply.github.com>
2022-04-14 13:04:43 +0300

    Add files via upload
    Dynamic OrderBy

---
+++ IHasOrderBy.cs
@@ -0,0 +1,8 @@
+﻿namespace DRR.Application.Common.Extensions
+{
+    public interface IHasOrderBy
+    {
+        //Contains string expression like ("Name ASC, ChildEntity.Name DESC")
+        public string OrderByExpression { get; set; }
+    }
+}
